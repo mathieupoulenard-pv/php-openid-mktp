0afd10a55dca69b5c79ae8be38cf54fa2c47ceb1
Mathieu Poulenard <m.poulenard@premierevision.com>
2020-02-24 22:56:57 +0100

    Add env var test

--- web/index.php
+++ web/index.php
@@ -19,7 +19,9 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+  return $app['twig']->render('index.twig', [
+  		'test' => getenv('TEST')
+  ]);
 });
 
 $app->run();
