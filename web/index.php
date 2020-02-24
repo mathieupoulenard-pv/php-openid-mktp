16fb1a8e9f7a0518065a3ac797d42e977dfdfde5
Mathieu Poulenard <m.poulenard@premierevision.com>
2020-02-24 23:56:21 +0100

    Update views

--- web/index.php
+++ web/index.php
@@ -19,12 +19,9 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 
 $client = HttpClient::create();
-$response = $client->request('GET', getenv('SF_LOGIN_URL').'/.well-known/openid-configuration');
-$statusCode = $response->getStatusCode();
-echo $statusCode;
-echo $response->getContent();
+$openidConf = $client->request('GET', getenv('SF_LOGIN_URL').'/.well-known/openid-configuration');
+$openidConf = $response->getContent();
 
-$openidConf = file_get_contents(getenv('SF_LOGIN_URL').'/.well-known/openid-configuration');
 // Our web handlers
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
