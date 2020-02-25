1eac62db0c33c5160ff5e9e877a8876589354cf9
Mathieu Poulenard <m.poulenard@premierevision.com>
2020-02-25 08:47:51 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -5,6 +5,8 @@ require('../vendor/autoload.php');
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Silex\Provider\Session\SessionListener;
 
 $app = new Silex\Application();
 $app['debug'] = true;
