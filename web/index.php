7e16acea76bc76c361d3e70c54d0ee9318fc12c4
mathieupoulenard-pv <61420076+mathieupoulenard-pv@users.noreply.github.com>
2020-11-18 10:30:28 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -46,7 +46,7 @@ $app->get('/', function(Request $request) use($app, $openidParams, $openidConf) 
   $app['monolog']->addDebug('logging output.');
 
  dump(getenv('ALLOWED_REFERERS'));
- dump(explode(getenv('ALLOWED_REFERERS'), ','));
+ dump(explode(getenv('ALLOWED_REFERERS'), ',')));
  dump(in_array($request->headers->get('referer'), explode(getenv('ALLOWED_REFERERS'), ','));
  // checklogin and autologin
   if (getenv('CHECKLOGIN') == 'true' && null == $userInfo && !$app['session']->get('checklogin') && true) {
